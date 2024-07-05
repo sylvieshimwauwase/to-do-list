126924a1367b0ebde519aa51ec91b150d4754558
sylvieshimwauwase <slvshimwa@gmail.com>
2024-07-05 11:46:31 +0200

    my to do list app updates

---
+++ src/page/index.jsx
@@ -0,0 +1,441 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container'>
+      <div className='sponsors'>
+        <div className='fw-logo-main-copy'>
+          <div className='shape' />
+          <div className='shape-1' />
+        </div>
+        <div className='group'>
+          <div className='group-2'>
+            <div className='vector' />
+            <div className='group-3'>
+              <div className='vector-4' />
+            </div>
+          </div>
+          <div className='group-5'>
+            <div className='vector-6' />
+          </div>
+        </div>
+        <div className='group-7'>
+          <div className='vector-8' />
+        </div>
+        <div className='circle-ci-svg'>
+          <div className='path' />
+        </div>
+      </div>
+      <div className='flex-row-a'>
+        <div className='about-us'>
+          <div className='frame'>
+            <div className='vector-9' />
+            <div className='about-us-a'>
+              <span className='about'>About</span>
+              <span className='us-at'> Us At</span>
+              <span className='about-b'> Z</span>
+              <span className='us-at-c'>ani.co </span>
+            </div>
+          </div>
+          <div className='frame-d'>
+            <span className='insurance-policy-text'>
+              Zani.co ensures her customers a good insurance policy to protect
+              them against risk inturn for regular payment of premium.
+            </span>
+          </div>
+        </div>
+        <div className='unsplash-rvvdgyucs'>
+          <div className='background-frame' />
+        </div>
+        <div className='insurance-text-body-text'>
+          <div className='frame-e'>
+            <div className='frame-f'>
+              <span className='you-pay-we-insure'>
+                You Pay,We Insure Your Deeds
+              </span>
+            </div>
+            <div className='frame-10'>
+              <span className='insurance-policy-text-11'>
+                Zani.co ensures her customers a good insurance policy to protect
+                them against risk inturn for regular payment of premium.
+              </span>
+            </div>
+          </div>
+          <div className='frame-12'>
+            <div className='frame-13'>
+              <div className='vector-14' />
+              <span className='registered-organisation'>
+                We are a registered organisation
+              </span>
+            </div>
+            <div className='frame-15'>
+              <div className='vector-16' />
+              <span className='flexible-life-insurance'>
+                Our life insurance policy is flexible
+              </span>
+            </div>
+            <div className='frame-17'>
+              <div className='vector-18' />
+              <span className='text-a'>
+                Over 5years in the insurance business
+              </span>
+            </div>
+            <div className='frame-19'>
+              <div className='vector-1a' />
+              <span className='excellent-deeds-protection'>
+                We provide excellent protections for your deeds
+              </span>
+            </div>
+          </div>
+          <div className='button'>
+            <span className='button-1b'>Register Now</span>
+          </div>
+        </div>
+      </div>
+      <div className='prefered-choice-body-text'>
+        <div className='prefered-choice'>
+          <div className='vector-1c' />
+          <div className='prefered-choice-1d'>
+            <span className='why'>Why </span>
+            <span className='we-are-your'>We Are Your </span>
+            <span className='why-1e'>Prefered </span>
+            <span className='we-are-your-1f'>Choice</span>
+          </div>
+        </div>
+        <div className='frame-20'>
+          <span className='zani-co-ensures'>
+            Zani.co ensures her customers a good insurance policy to protect
+            them against risk inturn for regular payment of premium.
+          </span>
+        </div>
+      </div>
+      <div className='flex-row-db'>
+        <div className='image-aligned-right' />
+        <div className='cards'>
+          <div className='card'>
+            <div className='frame-21'>
+              <div className='systems-shield-star-fill'>
+                <div className='systems-shield-star-fill-22'>
+                  <div className='group-23' />
+                </div>
+              </div>
+              <div className='ellipse' />
+            </div>
+            <div className='frame-24'>
+              <div className='frame-25'>
+                <span className='all-round-protection'>
+                  All-round Protection
+                </span>
+              </div>
+              <div className='frame-26'>
+                <span className='zani-co-ensures-27'>
+                  Zani.co ensures her customers a good insurance policy to
+                  protect them against risk inturn for regular payment of
+                  premium.
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className='card-28'>
+            <div className='frame-29'>
+              <div className='finance-wallet-fill'>
+                <div className='finance-wallet-fill-2a'>
+                  <div className='group-2b' />
+                </div>
+              </div>
+              <div className='ellipse-2c' />
+            </div>
+            <div className='frame-2d'>
+              <div className='frame-2e'>
+                <span className='high-saving-potentials'>
+                  High Saving Potentials
+                </span>
+              </div>
+              <div className='frame-2f'>
+                <span className='zani-co-ensures-30'>
+                  Zani.co ensures her customers a good insurance policy to
+                  protect them against risk inturn for regular payment of
+                  premium.
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className='card-31'>
+            <div className='frame-32'>
+              <div className='finance-hand-coin-fill'>
+                <div className='finance-hand-coin-fill-33'>
+                  <div className='group-34' />
+                </div>
+              </div>
+              <div className='ellipse-35' />
+            </div>
+            <div className='frame-36'>
+              <div className='frame-37'>
+                <span className='text-16'>5% Discount After 4years</span>
+              </div>
+              <div className='frame-38'>
+                <span className='zani-co-ensures-39'>
+                  Zani.co ensures her customers a good insurance policy to
+                  protect them against risk inturn for regular payment of
+                  premium.
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='services-body-text'>
+        <div className='frame-3a'>
+          <div className='vector-3b' />
+          <div className='services'>
+            <span className='services-3c'>Services</span>
+            <span className='we-render-at'> We Render At </span>
+            <span className='services-3d'>Z</span>
+            <span className='we-render-at-3e'>ani.co</span>
+          </div>
+        </div>
+        <div className='frame-3f'>
+          <span className='zani-co-ensures-customers'>
+            Zani.co ensures her customers a good insurance policy to protect
+            them against risk inturn for regular payment of premium.
+          </span>
+        </div>
+      </div>
+      <div className='flex-row-eea'>
+        <div className='card-40'>
+          <div className='frame-41'>
+            <div className='maps-car-fill'>
+              <div className='maps-car-fill-42'>
+                <div className='group-43' />
+              </div>
+            </div>
+          </div>
+          <div className='frame-44'>
+            <span className='car-insurance'>Car Insurance</span>
+          </div>
+          <div className='frame-45'>
+            <span className='zani-co-ensures-customers-46'>
+              Zani.co ensures her customers a good insurance policy to protect
+              them against risk inturn for regular payment of premium.
+            </span>
+          </div>
+        </div>
+        <div className='card-47'>
+          <div className='frame-48'>
+            <div className='finance-hand-heart-fill'>
+              <div className='finance-hand-heart-fill-49'>
+                <div className='group-4a' />
+              </div>
+            </div>
+          </div>
+          <div className='frame-4b'>
+            <span className='life-assurance'>Life Assurance</span>
+          </div>
+          <div className='frame-4c'>
+            <span className='zani-co-ensures-customers-4d'>
+              Zani.co ensures her customers a good insurance policy to protect
+              them against risk inturn for regular payment of premium.
+            </span>
+          </div>
+        </div>
+        <div className='card-4e'>
+          <div className='frame-4f'>
+            <div className='buildings-home-heart-fill'>
+              <div className='buildings-home-heart-fill-50'>
+                <div className='group-51' />
+              </div>
+            </div>
+          </div>
+          <div className='frame-52'>
+            <span className='home-insurance'>Home Insurance</span>
+          </div>
+          <div className='frame-53'>
+            <span className='zani-co-ensures-customers-54'>
+              Zani.co ensures her customers a good insurance policy to protect
+              them against risk inturn for regular payment of premium.
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className='clients-body-text'>
+        <div className='component'>
+          <div className='vector-55' />
+          <div className='client'>
+            <span className='our'>Our</span>
+            <span className='clients-knows-the'> Clients Knows The</span>
+            <span className='our-56'> Best</span>
+          </div>
+        </div>
+        <div className='frame-57'>
+          <span className='zani-co-ensures-customers-58'>
+            Zani.co ensures her customers a good insurance policy to protect
+            them against risk inturn for regular payment of premium.
+          </span>
+        </div>
+      </div>
+      <div className='flex-row-ab'>
+        <div className='ellipse-59' />
+        <div className='ellipse-5a' />
+        <div className='ellipse-5b' />
+        <div className='frame-5c'>
+          <div className='frame-5d'>
+            <div className='frame-5e'>
+              <span className='zani-insurance'>
+                Zani.co ensures her customers a good insurance policy to protect
+                them against risk inturn for regular payment of premium.
+              </span>
+            </div>
+          </div>
+          <div className='frame-5f'>
+            <span className='daniel-edwin'>Daniel Edwin</span>
+          </div>
+        </div>
+        <div className='frame-60'>
+          <div className='frame-61'>
+            <div className='frame-62'>
+              <span className='zani-insurance-63'>
+                Zani.co ensures her customers a good insurance policy to protect
+                them against risk inturn for regular payment of premium.
+              </span>
+            </div>
+          </div>
+          <button className='frame-64'>
+            <span className='austin-peters'>Austin Peters</span>
+          </button>
+        </div>
+        <div className='frame-65'>
+          <div className='frame-66'>
+            <div className='frame-67'>
+              <span className='zani-insurance-68'>
+                Zani.co ensures her customers a good insurance policy to protect
+                them against risk inturn for regular payment of premium.
+              </span>
+            </div>
+          </div>
+          <div className='frame-69'>
+            <span className='susana-james'>Susana James</span>
+          </div>
+        </div>
+      </div>
+      <div className='footer'>
+        <div className='vector-6a' />
+        <div className='reserved-rights'>
+          <span className='zani-co-all-right'>
+            @ Zani.co All Right Reserved
+          </span>
+        </div>
+        <div className='flex-row-d'>
+          <div className='logo'>
+            <div className='logo-6b'>
+              <div className='vector-6c' />
+              <span className='ani-co'>ani.co</span>
+            </div>
+          </div>
+          <div className='regroup'>
+            <div className='support'>
+              <span className='support-6d'>Support</span>
+              <span className='about-us-6e'>About Us</span>
+              <span className='services-6f'>Services</span>
+              <span className='management'>Management</span>
+              <span className='privacy-policy'>Privacy policy</span>
+            </div>
+            <div className='social'>
+              <span className='social-70'>Social</span>
+              <span className='facebook'>Facebook</span>
+              <span className='twitter'>Twitter</span>
+              <span className='linkdln'>Linkdln</span>
+              <span className='instagram'>Instagram</span>
+            </div>
+            <div className='contact-us'>
+              <span className='contact-us-71'>Contact Us</span>
+              <span className='www-zani-co-io'>www.zani.co.io</span>
+              <span className='zani-co-gmail-com'>Zani.co@gmail.com</span>
+              <span className='zani-co-yahool-com'>Zani.co001@yahool.com</span>
+            </div>
+          </div>
+          <div className='social-icons'>
+            <div className='logos-facebook-circle-fill'>
+              <div className='group-72' />
+            </div>
+            <div className='logos-twitter-fill'>
+              <div className='group-73' />
+            </div>
+            <div className='logos-linkedin-box-fill'>
+              <div className='group-74' />
+            </div>
+            <div className='logos-instagram-fill'>
+              <div className='group-75' />
+            </div>
+          </div>
+        </div>
+        <div className='text'>
+          <span className='zani-co-insurance-policy'>
+            Zani.co ensures her customers a good insurance policy to protect
+            them against risk inturn for regular payment of premium.
+          </span>
+        </div>
+      </div>
+      <div className='frame-76'>
+        <div className='frame-77'>
+          <div className='top-header'>
+            <div className='logo-78'>
+              <div className='vector-79' />
+              <span className='ani-co-7a'>ani.co</span>
+            </div>
+            <div className='links-aligned-right'>
+              <div className='links'>
+                <div className='home-link'>
+                  <span className='home'>Home</span>
+                </div>
+                <div className='about-link'>
+                  <span className='about-7b'>About</span>
+                </div>
+                <div className='service-link'>
+                  <span className='services-7c'>Services</span>
+                </div>
+                <div className='feature-link'>
+                  <span className='features'>Features</span>
+                </div>
+                <div className='contact-link'>
+                  <span className='contact-us-7d'>Contact Us</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='flex-row-b'>
+            <div className='hero-section-image' />
+            <div className='header-body-text-buttons'>
+              <div className='header-body-text'>
+                <div className='header-text'>
+                  <div className='stay-jiggy-insured'>
+                    <span className='stay'>Stay </span>
+                    <span className='jiggy'>Jiggy</span>
+                    <span className='while-we-get-you'> </span>
+                    <span className='stay-7e'>While We Get You </span>
+                    <span className='while-we-get-you-7f'>Insured</span>
+                  </div>
+                </div>
+                <div className='zani-co-insurance-policy-80'>
+                  <span className='frame-81'>
+                    Zani.co ensures her customers a good insurance policy to
+                    protect them against risk inturn for regular payment of
+                    premium.
+                  </span>
+                </div>
+              </div>
+              <div className='box-1f'>
+                <div className='button-82'>
+                  <span className='get-started'>Get Started</span>
+                </div>
+                <button className='button-83'>
+                  <span className='learn-more'>Learn more</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
